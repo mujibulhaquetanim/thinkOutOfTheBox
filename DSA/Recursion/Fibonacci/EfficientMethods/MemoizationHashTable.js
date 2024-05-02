534e52ff657ac9cf6bae9662ec5a66f4e9b60197
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-02 22:12:36 +0600

    fibonacci SOLVED by hashTable

--- DSA/Recursion/Fibonacci/EfficientMethods/MemoizationHashTable.js
+++ DSA/Recursion/Fibonacci/EfficientMethods/MemoizationHashTable.js
@@ -1,3 +1,12 @@
-function memoHashFibonacci(){
-    
+function memoHashFibonacci(n) {
+    const ht = { 0: 0, 1: 1 };
+    if (n in ht) {
+        return ht[n];
+    }
+    else {
+        ht[n] = memoHashFibonacci(n - 1) + memoHashFibonacci(n - 2);
+        return ht[n];
+    }
 }
+
+console.log(memoHashFibonacci(8));
