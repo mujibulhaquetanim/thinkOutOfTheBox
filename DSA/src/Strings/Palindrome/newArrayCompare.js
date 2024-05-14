d0cd7958d1497789546178efbe2ba6f39d55bed4
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-14 22:19:58 +0600

    solved via O(n) time complexity. (arrayPush)

--- DSA/src/Strings/Palindrome/newArrayCompare.js
+++ DSA/src/Strings/Palindrome/newArrayCompare.js
@@ -1,3 +1,15 @@
-function newArraycomparePalindrome(){
-    
+function newArraycomparePalindrome(defaultString) {
+    let newStringArray = [];
+    for (let i = defaultString.length; i >= 0; i--) {
+        newStringArray.push(defaultString[i]);
+    }
+    if (defaultString === newStringArray.join('')) return true;
+    else return false;
 }
+
+console.log(newArraycomparePalindrome('abcba'))
+console.log(newArraycomparePalindrome('aA'))
+console.log(newArraycomparePalindrome('a'))
+console.log(newArraycomparePalindrome('malayalam'))
+
+//pushing elements to an array is O(n) time complexity.
