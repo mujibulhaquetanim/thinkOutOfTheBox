028e05c20c4346435901df850977d618c4ca75c2
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-01 23:45:39 +0600

    something to number type coercion file added

---
+++ Tricky Code/Type Coercion/SomethingToNumber.js
@@ -0,0 +1,20 @@
+// 1. String to Number:
+    let str2Num = '5' - 2; // 3
+    console.log(result); // 3
+
+// 2. Number to String:
+    let Num2Str = 5 + '2'; // 52
+    console.log(result); // 52
+
+// 3. boolean to Number:
+    let bool2Num = true + 1; // 2
+    console.log(result); // 2
+
+// 4. Null to Number:
+    let Null2Num = null + 5; // 5
+    console.log(result); // 5
+
+// 5. Undefined to Number:
+    let result = undefined + 5; // NaN
+    console.log(result); // NaN
+
