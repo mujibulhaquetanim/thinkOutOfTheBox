88659f5ec9dabe8e88207858d80151f1a6723844
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-01 23:58:24 +0600

    type coercion on operators added

---
+++ Tricky Code/Type Coercion/Operators.js
@@ -0,0 +1,21 @@
+// 1. Equality Comparisons (==)
+    let eq2com = '5' == 5; // true
+    console.log(eq2com); // true
+
+// 2. Strict Equality Comparisons (===)
+    let StrictEq2com = '5' === 5; // false
+    console.log(StrictEq2com); // false
+
+// 3. Logical Operator:
+    let LogicalOp = 0 || 'Hello'; // 'Hello'
+    console.log(LogicalOp); // 'Hello'
+
+// 4. Unary Operators:
+    let Unary = +'123'; // 123
+    console.log(Unary); // 123
+
+// 5. Logical NOT (!)
+    let LogicalNot = !'Hello'; // false
+    console.log(LogicalNot); // false
+
+
