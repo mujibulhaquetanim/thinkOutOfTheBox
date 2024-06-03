0f9d4436a151ad1afb9decdfc83e789b395e4c30
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-03 21:18:04 +0600

    equality check & comparison check added

--- Tricky Code/Type Coercion/Operators.js
+++ Tricky Code/Type Coercion/Operators.js
@@ -18,4 +18,54 @@
     let LogicalNot = !'Hello'; // false
     console.log(LogicalNot); // false
 
+    // The reason is that an equality check and comparisons > <
+    // work differently.
+    // Comparisons convert null to a number, treating it as 0.
+    // That's why (3) null 0 is true and (1) null > 0 is false.
+    //this kind of comparison/code is avoided for clean code.
+    console.log(null > 0); // false
+    console.log(null == 0); // false
+    console.log(null >= 0); // true
+
+    console.log(undefined < 0); // false
+    console.log(undefined == 0); // false
+    console.log(undefined >= 0); // false
+    console.log(undefined > 0); // false
+
+    console.log('' < 0); // false
+    console.log('' == 0); // false
+    console.log('' >= 0); // false
+    console.log('' > 0); // false
+
+    console.log(null == undefined); // true
+    console.log(null === undefined); // false
+
+    console.log(0 == ''); // true
+    console.log(0 === ''); // false
+
+    console.log(0 == null); // true
+    console.log(0 === null); // false
+
+    console.log(0 == undefined); // true
+    console.log(0 === undefined); // false
+
+    console.log(0 == NaN); // true
+    console.log(0 === NaN); // false
+
+    console.log(0 == false); // false
+    console.log(0 === false); // false
+
+    console.log(0 == true); // false
+    console.log(0 === true); // false
+
+    console.log(0 == '0'); // true
+    console.log(0 === '0'); // false
+
+    console.log(0 == 'false'); // false
+    console.log(0 === 'false'); // false
+
+    console.log(0 == 'true'); // false
+    console.log(0 === 'true'); // false
+
+    
 
