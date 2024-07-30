4fd25ee9b5bfbc13a3b09226038759f8402a1067
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-30 23:55:59 +0600

    func in js as like a variable

---
+++ Tricky Code/weird JS/funcOverride.js
@@ -0,0 +1,18 @@
+// in js, func are treated like a variable.
+function show() { }
+
+show = "the show is overwritten";
+console.log(show) // the show is overwritten
+// show(); //error: show is not a function
+
+//another example:
+var foo = "ayaain"
+
+function showName() {
+    foo = "local variable"
+    return;
+    function foo() { } //this foo will be attached to showName in the compilation time, and then, foo= "" will reassigned it to "local variable" instead of function. but if we remove foo() then foo= "" will work
+}
+showName();
+console.log(foo)
+
