f1d3ad6fc3701036e79e6f08a140983a9f434c3b
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-25 23:50:31 +0600

    example: what "this" refers in arrow & regular funcs an a method in an obj.

--- Tricky Code/weird JS/this.js
+++ Tricky Code/weird JS/this.js
@@ -7,3 +7,20 @@ function getLength() {
 let arr = [getLength, 6, 9]
 
 console.log(arr[0]()); //result: 3. because this result depends on where the function is called, as this was called in the arr[0], so it will return the length of the array, which is 3.
+
+RootObj = {
+    name: "RootObj",
+    ReferRootObj() {
+        let name = "RootFunc"
+        return this.name;      
+    },
+
+    LocalObj: () => {
+        let name = "LocalFunc";
+        return this.name;
+    }
+}
+
+
+console.log(RootObj.ReferRootObj())//RootObj, as it refers to immediate parent.
+console.log(RootObj.LocalObj()) //undefined, as it refers to global obj, where it was defined.
