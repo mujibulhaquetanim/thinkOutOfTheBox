8768672714c9da6d40ad78627644611b3b978632
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-07 22:21:59 +0600

    `fix` the logic and refactor comments in l`ruCache` class for clarity and consistency

--- Operating System/Caching/lru_page_replacement.js
+++ Operating System/Caching/lru_page_replacement.js
@@ -1,4 +1,6 @@
 class lruCache{
+    //delete key and set it again in the cache to make it most frequent used. it will set in the last item of the cache.
+    // remove the first item of the store if the cache is full.
     constructor(capacity){
         // take process size
         // make cache to mimic ram
@@ -7,17 +9,23 @@ class lruCache{
     }
 
     get(key){
-        // check whether the key is in the cache or not, if cache has it, then remove it and then set it again.
-        // if no in cache then set it.
-        // return the key.
+        // returns the upon caching the cache:
+        /*
+        - check whether the key is in the cache or not,
+        - if not in the cache then return -1 meaning key doesn't exist.
+        - if cache has it, then remove it and store it in a variable
+        - then set it again.
+        - return the variable.
+        */
     }
 
     put(key, value){
-        // check the key is in the cache or not,
-        // if is in the cache then delete it.
-        // elseif, check if the cache size is equal to the capacity; if equal, remove the first key/index of the map
-        // set the new key to the map.
+        // delete the key to the cache if already in the cache and set it again and if the capacity size is equal to the size of the cache then delete it 
+        /*
+        - check the key is in the cache or not,
+        - if is in the cache then delete it.
+        - elseif, check if the cache size is equal to the capacity; if equal, remove the first key/index of the map
+        - set the new key to the map.
+        */
     }
 }
-
-const cache = lruCache(capacity)
